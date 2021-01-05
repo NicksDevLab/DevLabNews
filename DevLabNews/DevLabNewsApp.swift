ece79aa01c834a08039307cf76dc28748ab355e0
NicksDevLab <nicholaschurch@ymail.com>
2021-01-05 11:33:17 -0500

    NA

--- DevLabNews/DevLabNewsApp.swift
+++ DevLabNews/DevLabNewsApp.swift
@@ -10,13 +10,22 @@ import SwiftUI
 @main
 struct DevLabNewsApp: App {
   
+  @StateObject var dataProvider = DataProvider()
   let persistenceController = PersistenceController.shared
 
   var body: some Scene {
     WindowGroup {
       ContentView()
       .environment(\.managedObjectContext, persistenceController.container.viewContext)
+      .environmentObject(dataProvider)
     }
   }
   
 }
+
+
+class DataProvider: ObservableObject {
+  @Published var showSafariView = false
+}
+
+
